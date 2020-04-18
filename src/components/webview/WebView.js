7a7b742347af3fea7a2b60ce5724d4727899b60e
mayank8aug <mayankkumar@lenskart.in>
2020-04-18 18:50:35 +0530

    Added webview components

---
+++ src/components/webview/WebView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { WebView } from 'react-native-webview';
+
+function WebView() {
+    return (
+        <WebView
+            src="https://github.com/mayank8aug/ReactNative-Redux-Saga-Boilerplate"
+        />
+    );
+}
+
+export default WebView;
--- src/navigators/AppNavigator.js
+++ src/navigators/AppNavigator.js
@@ -4,13 +4,15 @@ import Drawer from './Drawer';
 import Home from '../components/home/Home';
 import Login from '../components/login/Login';
 import ContactUs from '../components/contact/ContactUs';
+import WebView from '../components/webview/WebView';
 
 const Navigator = createStackNavigator(
   {
     Drawer: { screen: Drawer },
     Home: { screen: Home },
     Login: { screen: Login },
-    ContactUs: { screen: ContactUs }
+    ContactUs: { screen: ContactUs },
+    WebView: { screen: WebView }
   },
   {
     initialRouteName: 'Drawer',
